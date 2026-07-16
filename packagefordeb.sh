d1471c9152464e6daa37e7de5d905ee59c748cfe
BiuCheung <22152306+BiuCheung@users.noreply.github.com>
2026-07-16 17:29:53 +0800

    Update packagefordeb.sh

--- packagefordeb.sh
+++ packagefordeb.sh
@@ -1,13 +1,14 @@
 #!/var/jb/usr/bin/bash
 echo "开始删除"
-rm -r Packages
-rm -r Packages.bz2
-rm -r Packages.gz
+rm Packages Packages.*
 echo "删除成功"
 echo "开始打包"
 dpkg-scanpackages --multiversion debs >> Packages
 cat Packages | bzip2 > Packages.bz2
 cat Packages | gzip > Packages.gz
+cat Packages | xz > Packages.xz
+cat Packages | lzma > Packages.lzma
+cat Packages | zstd > Packages.zst
 echo "打包成功"
 
 echo "开始推送"
